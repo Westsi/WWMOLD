c96cf903b456dcd4851cf64efd34d49a337ee70d
Westsi <jamie@rockyroost.com>
2022-01-28 17:22:08 +0000

    idk

---
+++ src/main/java/net/westsi/wwm/WWMClient.java
@@ -0,0 +1,13 @@
+package net.westsi.wwm;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class WWMClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
